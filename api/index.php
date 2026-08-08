bae463465a1431244a00174f0ee891e6cd75199c
firstian1000 <firstian1000@gmail.com>
2026-08-09 00:38:36 +0700

    Fix Laravel Vercel serverless runtime

--- api/index.php
+++ api/index.php
@@ -1,2 +1,30 @@
-<?php  
-require __DIR__ . "/../public/index.php";
+<?php
+
+putenv('VERCEL=1');
+$_ENV['VERCEL'] = '1';
+$_SERVER['VERCEL'] = '1';
+
+$storageDirs = [
+    '/tmp/storage/app/public',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/framework/views',
+    '/tmp/storage/logs',
+    '/tmp/bootstrap/cache',
+];
+
+foreach ($storageDirs as $dir) {
+    if (! is_dir($dir)) {
+        @mkdir($dir, 0755, true);
+    }
+}
+
+putenv('LARAVEL_STORAGE_PATH=/tmp/storage');
+putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
+putenv('APP_CONFIG_CACHE=/tmp/bootstrap/cache/config.php');
+putenv('APP_EVENTS_CACHE=/tmp/bootstrap/cache/events.php');
+putenv('APP_PACKAGES_CACHE=/tmp/bootstrap/cache/packages.php');
+putenv('APP_ROUTES_CACHE=/tmp/bootstrap/cache/routes.php');
+putenv('APP_SERVICES_CACHE=/tmp/bootstrap/cache/services.php');
+
+require __DIR__ . '/../public/index.php';
