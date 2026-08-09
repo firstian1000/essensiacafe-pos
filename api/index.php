01707cc03dff08a5669b2ef113438166cabdeea4
firstian1000 <firstian1000@gmail.com>
2026-08-09 21:49:01 +0700

    edit index.php

--- api/index.php
+++ api/index.php
@@ -1,3 +1,19 @@
 <?php
 
+// 1. Buat direktori sementara (tmp) yang diwajibkan oleh Laravel
+$directories = [
+    '/tmp/bootstrap/cache',
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs'
+];
+
+foreach ($directories as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0777, true);
+    }
+}
+
+// 2. Teruskan request ke file public/index.php milik Laravel
 require __DIR__ . '/../public/index.php';
