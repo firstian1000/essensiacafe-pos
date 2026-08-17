893e1a14fa68adffdeaf760cdff1b5f71a3be550
firstian1000 <firstian1000@gmail.com>
2026-08-18 01:29:37 +0700

    Update mobile cart label and cashier button view

--- resources/views/customer/menu/menu.blade.php
+++ resources/views/customer/menu/menu.blade.php
@@ -230,7 +230,9 @@
         <div class="float-bar-total">Rp {{ number_format($floatTotal,0,',','.') }}</div>
     </div>
     <a href="{{ route('cart.index') }}" class="float-bar-btn">
-        Lihat Keranjang <i class="bi bi-arrow-right"></i>
+        <span class="float-bar-btn-text">Lihat Keranjang</span>
+        <span class="float-bar-btn-mobile-text">Lanjutkan</span>
+        <i class="bi bi-arrow-right"></i>
     </a>
 </div>
 @endif
@@ -387,7 +389,9 @@ function updateFloatCart(data) {
             <div class="float-bar-total">${rupiah(data.cart_total)}</div>
         </div>
         <a href="${cartUrl}" class="float-bar-btn">
-            Lihat Keranjang <i class="bi bi-arrow-right"></i>
+            <span class="float-bar-btn-text">Lihat Keranjang</span>
+            <span class="float-bar-btn-mobile-text">Lanjutkan</span>
+            <i class="bi bi-arrow-right"></i>
         </a>
     `;
 }
